6e454ba249c8ee80e3439f962261867153582778
Mark Beech <mbeech@mark-beech.co.uk>
2021-04-15 22:24:02 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,27 @@
+# ionic-vue-item-list
+An example of a nicely styled item list in Ionic Vue
+
+Based on [ionic-vue-item-list](https://github.com/alanmontgomery/ionic-react-item-list) by [@alanmontgomery](https://github.com/alanmontgomery/ionic-react-item-list)
+
+<!-- ![Ionic React Item List](https://repository-images.githubusercontent.com/358203630/3953e780-9ddf-11eb-9c0b-da2dde02dbfb) -->
+
+### Included in this Ionic Vue Template/UI
+* Custom animations
+* Custom Toolbar and Item
+* Removing an Item
+* Dynamic search list
+* Use of stock Ionic components
+* SASS Modules
+* Ionic CSS utilities
+
+### To run
+
+```javascript
+npm install
+ionic serve
+```
+
+Alternatively, you can add the iOS, Android platform and run natively.
+
+<!-- # Are you on Twitter? Lets connect [@93alan](https://twitter.com/93alan)
+# Have you checked out Ionic React Hub yet? [Ionic React Hub](https://ionicreacthub.com) -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,268 @@
 <template>
-  <ion-page>
+  <ion-page class="ion-page page">
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-buttons slot="start">
+          <ion-button color="light">
+            <ion-icon :icon="chevronBack"></ion-icon>
+            &nbsp;Employee List
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+  
+      <div class="mainContent">
+        <ion-card-subtitle>{{ results.length }} {{ (results.legnth === 1) ? "employee" : "employees" }} found</ion-card-subtitle>
+        <ion-searchbar @keyup="search($event)" @keypress="search($event)" placeholder="Search..." icon="search" slot="end" />
+
+        <ion-list>
+          <ion-item :id="'employeeItem_' + employee.id" v-for="employee in results" :key="employee.id" class="employeeItem animate__animated animate__fadeIn" lines="none">
+            <img :src="employee.avatar" alt="employee avatar" />
+
+            <ion-label>
+              <h2>{{ employee.name }}</h2>
+              <p>{{ employee.title }}</p>
+            </ion-label>
+
+            <ion-button @click="remove(employee.id)">
+              <ion-icon :icon="trashOutline" />
+            </ion-button>
+          </ion-item>
+        </ion-list>
       </div>
+
     </ion-content>
+
+    <ion-footer>
+      <ion-button expand="block">Add new employee</ion-button>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonButton, IonButtons, IonCardSubtitle, IonContent, IonFooter, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonPage, IonSearchbar, IonToolbar } from '@ionic/vue';
+import { chevronBack, trashOutline } from 'ionicons/icons';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
+    IonButton,
+    IonButtons,
+    IonCardSubtitle,
     IonContent,
+    IonFooter,
     IonHeader,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonList,
     IonPage,
-    IonTitle,
+    IonSearchbar,
     IonToolbar
+  },
+  setup() {
+    const employees = ref([
+      {
+        id: 1,
+        name: "Mark Beech",
+        title: "Senior Web Developer",
+        avatar: "https://pbs.twimg.com/profile_images/378800000775628335/857bcaffb820e5b6bdd2e83a4b56cf89_400x400.jpeg"
+      },
+      {
+        id: 2,
+        name: "Alan Montgomery",
+        title: "Mobile Team Lead",
+        avatar: "https://pbs.twimg.com/profile_images/1349059994747076610/8dWvipvu_400x400.jpg"
+      },
+      {
+        id: 3,
+        name: "Max Lynch",
+        title: "CEO | Co Founder",
+        avatar: "https://pbs.twimg.com/profile_images/1318970727173885953/bln98FNj_400x400.jpg"
+      },
+      {
+        id: 4,
+        name: "Mike Hartington",
+        title: "Senior Dev Rel",
+        avatar: "https://pbs.twimg.com/profile_images/1084993841898446849/DJ8XtR6L_400x400.jpg"
+      },
+      {
+        id: 5,
+        name: "Matt Netkow",
+        title: "Head of Product Marketing",
+        avatar: "https://pbs.twimg.com/profile_images/1323383930150621187/GKc0nVzi_400x400.jpg"
+      },
+      {
+        id: 6,
+        name: "Ben Sperry",
+        title: "CDO | Co Founder",
+        avatar: "https://pbs.twimg.com/profile_images/1328390491126308864/jHHgl5Dm_400x400.jpg"
+      },
+      {
+        id: 7,
+        name: "Liam DeBeasi",
+        title: "Software Engineer",
+        avatar: "https://pbs.twimg.com/profile_images/1105953692669366273/ZNK4lRAJ_400x400.jpg"
+      }
+    ])
+
+    const results = ref(employees.value)
+
+    const remove = (id: any) => {
+      document.getElementById(`employeeItem_${ id }`)?.classList.add("animate__slideOutRight")
+
+      setTimeout(() => {
+        const tempEmployees = employees.value
+        const newEmployees = tempEmployees.filter(e => e.id !== id)
+        results.value = newEmployees
+        employees.value = newEmployees
+      }, 700)
+    }
+
+    const search = (e: any) => {
+      const searchTerm = e.currentTarget.value;
+
+      if (searchTerm !== "") {
+        const searchTermLower = searchTerm.toLowerCase()
+        const newResults = employees.value.filter(e => e.name.toLowerCase().includes(searchTermLower))
+        results.value = newResults
+      } else {
+        results.value = employees.value
+      }
+    }
+
+    return { employees, results, search, remove, chevronBack, trashOutline }
   }
-});
+})
 </script>
 
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+<style lang="scss">
+.page {
+
+  ion-header {
+
+    background-color: #5a55ca;
+  }
+
+  ion-toolbar {
+
+    --border-style: none;
+    --background: #5a55ca;
+    --color: white;
+    --min-height: 8rem;
+
+    // --stripe: #645fd1;
+    --stripe: #5a55ca;
+    --bg: #645fd1;
+    --background: transparent;
+    background: linear-gradient(135deg, var(--bg) 25%, transparent 25%) -50px 0,
+      linear-gradient(225deg, var(--bg) 25%, transparent 25%) -50px 0,
+      linear-gradient(315deg, var(--bg) 25%, transparent 25%),
+      linear-gradient(45deg, var(--bg) 25%, transparent 25%);
+    background-size: 100px 100px;
+    background-color: var(--stripe);
+
+    ion-button {
+      font-size: 1.2rem;
+      font-weight: 800;
+    }
+  }
+
+  ion-content {
+    --background: #5a55ca;
+    overflow: hidden;
+  }
+
+  ion-card-subtitle {
+    padding-left: 1.2rem;
+    margin-top: 1.9rem;
+    padding-bottom: 0.3rem;
+    --color: #5a55ca;
+  }
+
+  ion-searchbar {
+    --border-radius: 10px;
+    --background: white;
+    --color: black;
+    min-height: 5rem;
+    padding: 1rem;
+    padding-top: 0;
+    margin-top: -1rem;
+  }
+
+  ion-list {
+    background-color: #e7edfb;
+  }
+
+  ion-footer {
+    background-color: #e7edfb;
+    padding: 1rem;
+
+    ion-button {
+      --background: #5a55ca;
+      --background-focused: #6f6bbb;
+      --background-activated: #6f6bbb;
+      --padding-top: 1rem;
+      --padding-bottom: 1rem;
+      --padding-start: 0.75rem;
+      --padding-end: 0.75rem;
+      height: 3.5rem;
+      font-weight: 700;
+    }
+  }
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+.mainContent {
+  background-color: #e7edfb;
+  height: 100%;
+  width: 100%;
+  overflow: scroll !important;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
+.employeeItem {
+  --border-radius: 10px;
+  --padding-start: 1rem;
+  --padding-end: 1rem;
+  --padding-top: 1rem;
+  --padding-bottom: 1rem;
+  padding: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  min-height: 5rem;
 
-#container a {
-  text-decoration: none;
+  img {
+    width: 3rem;
+    border-radius: 10px;
+    border: 2px solid #e7edfb;
+  }
+
+  ion-label {
+    padding-left: 1.2rem;
+
+    h2 {
+      font-weight: 600;
+      letter-spacing: -0.02rem;
+    }
+
+    p {
+      letter-spacing: -0.03rem;
+    }
+  }
+
+  ion-button {
+    --background: #5a55ca;
+    --background-focused: #6f6bbb;
+    --background-activated: #6f6bbb;
+    --padding-top: 1rem;
+    --padding-bottom: 1rem;
+    --padding-start: 0.75rem;
+    --padding-end: 0.75rem;
+    margin-top: -0.2rem;
+  }
 }
 </style>
